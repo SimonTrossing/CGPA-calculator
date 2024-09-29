6f64ba0cd280ff8be7910271d93e902e90fd0f50
SimonTrossing <simon.trossing@student.tabyenskilda.se>
2024-09-29 23:17:17 +0200

    använde klass

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-def get_courses():
-    while True:
-        try:
-            courses = int(input("How many courses do you take?"))
-            break
-        except ValueError:
-            print("it needs to be a non-decimal number")
-    courses_dict = {}
-    for i in range(courses):
-        course = input(f"what is your {i+1} course?")
+class GPA_calc:
+    def __init__(self):
+        self.grades = self.get_courses()
+
+    def get_courses(slef):
         while True:
             try:
-                grade = int(input(f"What was your grade in {course}?"))
+                courses = int(input("How many courses do you take?"))
                 break
             except ValueError:
                 print("it needs to be a non-decimal number")
-        courses_dict.update({course:grade})
-    return courses_dict
+        courses_dict = {}
+        for i in range(courses):
+            course = input(f"what is your {i+1} course?")
+            while True:
+                try:
+                    grade = int(input(f"What was your grade in {course}?"))
+                    break
+                except ValueError:
+                    print("it needs to be a non-decimal number")
+            courses_dict.update({course:grade})
+        return courses_dict
 
 
-def calculator(grades):
-    courses = list(grades.keys())
-    GPA = 0
-    for i in range(len(courses)):
-        GPA += grades[courses[i]]
-    GPA = GPA/len(courses)
-    return GPA
+    def calculator(self):
+        courses = list(self.grades.keys())
+        GPA = 0
+        for i in range(len(courses)):
+            GPA += self.grades[courses[i]]
+        GPA = GPA/len(courses)
+        return GPA
 
 
-def weaklink(grades):
-    sorted_grades = sorted(grades)
-    return grades[sorted_grades[0]], sorted_grades[0]
+    def weaklink(self):
+        sorted_grades = sorted(self.grades.keys())
+        return self.grades[sorted_grades[-1]], sorted_grades[-1]
 
 
 def main():
-    print(weaklink(get_courses()))
-    print(f"your GPA is {calculator(get_courses())}")
-    
+    my_GPA_calc = GPA_calc()
+    grade, subject = my_GPA_calc.weaklink()
+    print(f"your Weakest subject was {subject} with a {grade}")
+    print(f"your GPA is {my_GPA_calc.calculator()}")
 main()
 
