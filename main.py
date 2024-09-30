15d067893552575ea15f78685b808fcef849734e
SimonTrossing <simon.trossing@student.tabyenskilda.se>
2024-09-30 14:23:01 +0200

    improved the design

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class GPA_calc:
     def __init__(self):
+        self.courses = []
         self.grades = self.get_courses()
         self.grade_score = {"A+":4,"A":4,"A-":3.7,"B+":3.3,"B":3,"B-":2.7,"C+":2.3,"C":2,"C-":1.7,"D+":1.3,"D":1,"F":0}
-        self.courses = ""
+       
 
     def get_courses(self):
         while True:
@@ -14,13 +15,13 @@ class GPA_calc:
         courses_dict = {}
         for i in range(courses):
             course = input(f"what is your {i+1} course?")
+            self.courses.append(course)
             grade = input(f"What was your grade in {course}?")
             courses_dict.update({course:grade})
         return courses_dict
 
 
     def calculator(self):
-        self.courses = list(self.grades.keys())
         GPA = 0
         for i in range(len(self.courses)):
             GPA += self.grade_score[self.grades[self.courses[i]]]
