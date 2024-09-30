07b97b8b7ef454748f23a94f536303f8644c53ae
SimonTrossing <simon.trossing@student.tabyenskilda.se>
2024-09-30 13:43:37 +0200

    Corrected it to the correct point system

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class GPA_calc:
     def __init__(self):
         self.grades = self.get_courses()
+        self.grade_score = {"A+":4,"A":4,"A-":3.7,"B+":3.3,"B":3,"B-":2.7,"C+":2.3,"C":2,"C-":1.7,"D+":1.3,"D":1,"F":0}
 
-    def get_courses(slef):
+    def get_courses(self):
         while True:
             try:
                 courses = int(input("How many courses do you take?"))
@@ -12,12 +13,7 @@ class GPA_calc:
         courses_dict = {}
         for i in range(courses):
             course = input(f"what is your {i+1} course?")
-            while True:
-                try:
-                    grade = int(input(f"What was your grade in {course}?"))
-                    break
-                except ValueError:
-                    print("it needs to be a non-decimal number")
+            grade = input(f"What was your grade in {course}?")
             courses_dict.update({course:grade})
         return courses_dict
 
@@ -26,7 +22,7 @@ class GPA_calc:
         courses = list(self.grades.keys())
         GPA = 0
         for i in range(len(courses)):
-            GPA += self.grades[courses[i]]
+            GPA += self.grade_score[self.grades[courses[i]]]
         GPA = GPA/len(courses)
         return GPA
 
