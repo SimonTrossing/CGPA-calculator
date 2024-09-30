c255f16fdd691ceba9f4835fddd39d9ebac482a4
SimonTrossing <simon.trossing@student.tabyenskilda.se>
2024-09-30 14:16:23 +0200

    removed a non-funktioning function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class GPA_calc:
     def __init__(self):
         self.grades = self.get_courses()
         self.grade_score = {"A+":4,"A":4,"A-":3.7,"B+":3.3,"B":3,"B-":2.7,"C+":2.3,"C":2,"C-":1.7,"D+":1.3,"D":1,"F":0}
+        self.courses = ""
 
     def get_courses(self):
         while True:
@@ -19,23 +20,16 @@ class GPA_calc:
 
 
     def calculator(self):
-        courses = list(self.grades.keys())
+        self.courses = list(self.grades.keys())
         GPA = 0
-        for i in range(len(courses)):
-            GPA += self.grade_score[self.grades[courses[i]]]
-        GPA = GPA/len(courses)
+        for i in range(len(self.courses)):
+            GPA += self.grade_score[self.grades[self.courses[i]]]
+        GPA = GPA/len(self.courses)
         return GPA
-
-
-    def weaklink(self):
-        sorted_grades = sorted(self.grades.keys())
-        return self.grades[sorted_grades[-1]], sorted_grades[-1]
 
 
 def main():
     my_GPA_calc = GPA_calc()
-    grade, subject = my_GPA_calc.weaklink()
-    print(f"your Weakest subject was {subject} with a {grade}")
     print(f"your GPA is {my_GPA_calc.calculator()}")
 main()
 
